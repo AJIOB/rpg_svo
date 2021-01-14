04d105693779fb604eafab37fec3c965151dcd1f
AJIOB <aleksandr9809@gmail.com>
2021-01-15 00:02:13 +0300

    Updating .sh scripts execution flags run_host should resolve paths nicely

--- run_host.sh
+++ run_host.sh
@@ -2,5 +2,8 @@
 # Datasets must be located near rpg_svo folder
 
 echo "Start host"
-cd "$(dirname "$0/../")"
-docker run --rm -it ajiob/svo-run:1 ./rpg_svo/run.sh
+cd "$(dirname "$0")/.."
+
+docker run --rm -it -v "$(pwd):/q" "-w/q" ajiob/svo-run:1 ./rpg_svo/run.sh
+
+cd -
